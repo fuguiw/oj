0e8fcf49c5e6b09b941599e5447fbcc25b4792a9
Kyne2007 <4019532@qq.com>
2014-04-29 17:49:54 +0800

    杂题

---
+++ soj/4355.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int n;
+
+long long count(int num)
+{
+    long long cnt;
+    long long w;
+    long long low = 0, cur = 0, high = 0;
+
+    cnt = 0;
+    for (w = 1; n / w; w *= 10) {
+        low = n - (n / w) * w;
+        cur = n / w % 10;
+        high = n / w / 10;
+        if (cur < num) cnt += high * w;
+        if (cur == num) cnt += high * w + low + 1;
+        if (cur > num) cnt += (high + 1) * w;
+        if (num == 0) {
+            if (cur == num) cnt -= w;
+            if (cur > num) cnt -= w;
+        }
+    }
+    return cnt;
+}
+
+int main()
+{
+    while (cin >> n) {
+        for (int i = 0; i <= 9; i++)
+            cout << count(i) << endl;
+    }
+    return 0;
+}
