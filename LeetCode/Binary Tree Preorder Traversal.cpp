530243bee6f0c362b3a24191a793bb550225a4bd
Kyne <kyne2007@icloud.com>
2015-03-12 23:38:05 +0800

    Tree, Stack

--- LeetCode/Binary Tree Preorder Traversal.cpp
+++ LeetCode/Binary Tree Preorder Traversal.cpp
@@ -1,29 +1,57 @@
+#include <iostream>
+#include <vector>
+#include <stack>
+using namespace std;
 /**
  * Definition for binary tree
- * struct TreeNode {
- *     int val;
- *     TreeNode *left;
- *     TreeNode *right;
- *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
- * };
  */
-class Solution {
-private:
-	vector<int> traversal; 
-	void _preorderTraversal(TreeNode *root) {
-		if (!root)
-			return ;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
 
-		traversal.push_back(root->val);
-		if (root->left)
-			_preorderTraversal(root->left);
-		if (root->right)
-			_preorderTraversal(root->right);
-	}
+class Solution {
 public:
     vector<int> preorderTraversal(TreeNode *root) {
-		traversal.clear();
-		_preorderTraversal(root);
-		return traversal;
+        vector<int> v;
+        stack<TreeNode *> st;
+        TreeNode *cur;
+
+        if (root)
+            st.push(root);
+
+        while (!st.empty()) {
+            cur = st.top();
+            st.pop();
+            v.push_back(cur->val);
+            
+            if (cur->right)
+                st.push(cur->right);
+            if (cur->left)
+                st.push(cur->left);
+        }
+
+        return v;
     }
 };
+
+int main()
+{
+    Solution sol;
+    TreeNode n1(1);
+    TreeNode n2(2);
+    TreeNode n3(3);
+    vector<int> v;
+
+    n1.right = &n2;
+    n2.left = &n3;
+
+    v = sol.preorderTraversal(&n1);
+
+    for (auto i : v)
+        cout << i << endl;
+
+    return 0;
+}
