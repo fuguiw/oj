a064ae708273c4e3021a833bb4f478382f3b35e9
Kyne <kyne2007@icloud.com>
2015-04-28 15:19:02 +0800

    Tree, DFS

--- LeetCode/Validate Binary Search Tree.cpp
+++ LeetCode/Validate Binary Search Tree.cpp
@@ -3,49 +3,50 @@
 using namespace std;
 
 struct TreeNode {
-	int val;
-	TreeNode *left;
-	TreeNode *right;
-	TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
 };
 
 class Solution {
 private:
-	bool _isValidBST(TreeNode *root, bool minFlag, int minVal, bool maxFlag, int maxVal) {
-		if (minFlag && root->val <= minVal)
-			return false;
-		if (maxFlag && root->val >= maxVal)
-			return false;
-		if (root->left && !_isValidBST(root->left, minFlag, minVal, true, root->val))
-			return false;
-		if (root->right && !_isValidBST(root->right, true, root->val, maxFlag, maxVal))
-			return false;
-		return true;
-	}
+    bool isValidBST(TreeNode *root, long long &last_val) {
+        if (root == nullptr)
+            return true;
+
+        if (isValidBST(root->left, last_val) == false)
+            return false;
+
+        if (root->val <= last_val)
+            return false;
+
+        last_val = root->val;
+
+        if (isValidBST(root->right, last_val) == false)
+            return false;
+
+        return true;
+    }
 
 public:
     bool isValidBST(TreeNode *root) {
-		if (!root)
-			return true;
-		if (root->left && !_isValidBST(root->left, false, INT_MIN, true, root->val))
-			return false;
-		if (root->right && !_isValidBST(root->right, true, root->val, false, INT_MAX))
-			return false;
-		return true;
+        long long last_val = static_cast<long long>INT_MIN - 1;
+        return isValidBST(root, last_val);
     }
 };
 
 int main()
 {
-	TreeNode n1(1);
-	TreeNode n2(2);
-	TreeNode n3(3);
-	Solution sol;
+    TreeNode n1(1);
+    TreeNode n2(2);
+    TreeNode n3(3);
+    Solution sol;
 
-	n2.left = &n1;
-	n2.right = &n3;
+    n2.left = &n1;
+    n2.right = &n3;
 
-	cout << sol.isValidBST(&n2) << endl;
+    cout << sol.isValidBST(&n2) << endl;
 
-	return 0;
+    return 0;
 }
