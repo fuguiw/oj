2275cc2f3422f2eb919d442e49b9ad3f5f56dd10
Kyne2007 <naiping.hz@qq.com>
2019-03-31 00:09:03 +0800

    array

--- LeetCode_Java/src/MaxArea.java
+++ LeetCode_Java/src/MaxArea.java
@@ -1,15 +1,19 @@
 public class MaxArea {
     public int maxArea(int[] height) {
-        int result = 0;
-        for (int i = 0; i < height.length; i++) {
-            for (int j = height.length - 1; j > i; j--) {
-                int area = (j - i) * Math.min(height[i], height[j]);
-                result = Math.max(result, area);
-                if (height[j] > height[i] || area < result) {
-                    break;
-                }
+        int area = 0;
+        int left = 0;
+        int right = height.length - 1;
+
+        while (left < right) {
+            area = Math.max(area, (right - left) * Math.min(height[left], height[right]));
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
             }
         }
-        return result;
+
+
+        return area;
     }
 }
