eb285069d0492a11725764678bfb48d803f0e8d4
Kyne2007 <naiping.hz@qq.com>
2019-04-02 22:50:43 +0800

    Linked list

---
+++ LeetCode_Java/src/ReverseList.java
@@ -0,0 +1,22 @@
+public class ReverseList {
+    /**
+     * Definition for singly-linked list.
+     * public class ListNode {
+     *     int val;
+     *     ListNode next;
+     *     ListNode(int x) { val = x; }
+     * }
+     */
+    public ListNode reverseList(ListNode head) {
+        ListNode curr = head;
+        ListNode pre = null;
+        while (curr != null) {
+            ListNode next = curr.next;
+            curr.next = pre;
+            pre = curr;
+            curr = next;
+        }
+
+        return pre;
+    }
+}
