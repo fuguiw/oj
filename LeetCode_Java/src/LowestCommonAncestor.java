31ea4fdc886eecd6fa4ba33ddd8377f87363c122
Kyne2007 <naiping.hz@qq.com>
2019-04-02 23:14:00 +0800

    Tree

---
+++ LeetCode_Java/src/LowestCommonAncestor.java
@@ -0,0 +1,29 @@
+public class LowestCommonAncestor {
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     *     int val;
+     *     TreeNode left;
+     *     TreeNode right;
+     *     TreeNode(int x) { val = x; }
+     * }
+     */
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val == p.val || root.val == q.val) {
+            return root;
+        }
+
+        TreeNode leftTree = lowestCommonAncestor(root.left, p, q);
+        TreeNode rightTree = lowestCommonAncestor(root.right, p, q);
+
+        if (leftTree != null && rightTree != null) {
+            return root;
+        }
+
+        return leftTree != null ? leftTree : rightTree;
+    }
+}
