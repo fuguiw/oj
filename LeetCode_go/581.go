900ff13dfd9d81fcf7b427bf38f9020bc32bcf51
Kyne2007 <naiping.hz@qq.com>
2019-11-29 23:39:37 +0800

    sort

---
+++ LeetCode_go/581.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func findUnsortedSubarray(nums []int) int {
+	s := make([]int, len(nums))
+	copy(s, nums)
+	sort.Slice(s, func(x, y int) bool {
+		return s[x] < s[y]
+	})
+
+	var i int
+	for i = 0; i < len(nums); i++ {
+		if nums[i] != s[i] {
+			break
+		}
+	}
+
+	for j := len(nums) - 1; j > i; j-- {
+		if nums[j] != s[j] {
+			return j - i + 1
+		}
+	}
+
+	return 0
+}
