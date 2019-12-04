8247a4789cc159d66bc06c34a8f5ee7daa64effe
Kyne2007 <naiping.hz@qq.com>
2019-12-05 00:04:27 +0800

    array

---
+++ LeetCode_go/48.go
@@ -0,0 +1,18 @@
+package main
+
+func rotate(matrix [][]int) {
+	n := len(matrix)
+	for i := 0; i < n/2; i++ {
+		for j := i; j < n-i-1; j++ {
+			tmp := matrix[j][n-i-1]
+			matrix[j][n-i-1] = matrix[i][j]
+			pre := tmp
+			tmp = matrix[n-i-1][n-j-1]
+			matrix[n-i-1][n-j-1] = pre
+			pre = tmp
+			tmp = matrix[n-j-1][i]
+			matrix[n-j-1][i] = pre
+			matrix[i][j] = tmp
+		}
+	}
+}
