ea85a5f851fb68a0b66d0466aa95a892de1550f9
Kyne2007 <naiping.hz@qq.com>
2020-02-13 00:10:22 +0800

    array

---
+++ LeetCode_go/j4.go
@@ -0,0 +1,19 @@
+package main
+
+func findNumberIn2DArray(matrix [][]int, target int) bool {
+	if len(matrix) == 0 {
+		return false
+	}
+	i, j := 0, len(matrix[0])-1
+	for i < len(matrix) && j >= 0 {
+		if matrix[i][j] == target {
+			return true
+		}
+		if matrix[i][j] > target {
+			j--
+		} else {
+			i++
+		}
+	}
+	return false
+}
