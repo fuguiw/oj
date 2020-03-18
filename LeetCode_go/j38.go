1767d52285155e5564261a56db27dfa5b548fcf3
Kyne2007 <naiping.hz@qq.com>
2020-03-18 22:38:58 +0800

    backtracing

--- LeetCode_go/j38.go
+++ LeetCode_go/j38.go
@@ -1,12 +1,12 @@
 package main
 
-func find(s string, ans *[]string, idx int, m *map[string]bool) {
+func find(s string, ans *[]string, idx int, m map[string]bool) {
 	if idx == len(s) {
-		if (*m)[s] {
+		if m[s] {
 			return
 		}
 		*ans = append(*ans, s)
-		(*m)[s] = true
+		m[s] = true
 		return
 	}
 
@@ -20,6 +20,6 @@ func find(s string, ans *[]string, idx int, m *map[string]bool) {
 func permutation(s string) []string {
 	var ans []string
 	m := make(map[string]bool)
-	find(s, &ans, 0, &m)
+	find(s, &ans, 0, m)
 	return ans
 }
