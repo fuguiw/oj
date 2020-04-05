c1a4d96af41c8db2f73ceacc9331e291769e2f60
Kyne2007 <naiping.hz@qq.com>
2020-04-05 20:42:35 +0800

    array

---
+++ LeetCode/31.cpp
@@ -0,0 +1,10 @@
+#include <algorithm>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        next_permutation(nums.begin(), nums.end());
+    }
+};
