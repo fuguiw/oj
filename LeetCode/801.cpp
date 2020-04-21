4ab7d7e059b44ee291de687035923248c4e540f6
Kyne2007 <naiping.hz@qq.com>
2020-04-21 23:49:38 +0800

    dp

---
+++ LeetCode/801.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+class Solution {
+public:
+    int minSwap(vector<int>& A, vector<int>& B) {
+        int n = A.size();
+        int dp[n][2];
+
+        dp[0][0] = 0;
+        dp[0][1] = 1;
+        for (int i = 1; i < n; i++) {
+            dp[i][0] = dp[i][1] = 4444;
+            if (A[i] > A[i-1] && B[i] > B[i-1]) {
+                dp[i][0] = min(dp[i][0], dp[i-1][0]);
+                dp[i][1] = min(dp[i][1], dp[i-1][1] + 1);
+            }
+            if (A[i] > B[i-1] && B[i] > A[i-1]) {
+                dp[i][0] = min(dp[i][0], dp[i-1][1]);
+                dp[i][1] = min(dp[i][1], dp[i-1][0] + 1);
+            }
+        }
+
+        return min(dp[n-1][0], dp[n-1][1]);
+    }
+};
