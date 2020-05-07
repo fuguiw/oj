98851cb010a0099c9ef460f52473003fe91c45e6
Kyne2007 <naiping.hz@qq.com>
2020-05-08 00:05:14 +0800

    linked list

---
+++ LeetCode_go/m02.08.go
@@ -0,0 +1,30 @@
+package main
+
+/**
+ * Definition for singly-linked list.
+ * type ListNode struct {
+ *     Val int
+ *     Next *ListNode
+ * }
+ */
+func detectCycle(head *ListNode) *ListNode {
+	slow, fast := head, head
+	for fast != nil && fast.Next != nil {
+		slow = slow.Next
+		fast = fast.Next.Next
+		if slow == fast {
+			break
+		}
+	}
+
+	if fast == nil || fast.Next == nil {
+		return nil
+	}
+
+	for slow = head; fast != nil && slow != fast; {
+		fast = fast.Next
+		slow = slow.Next
+	}
+
+	return fast
+}
