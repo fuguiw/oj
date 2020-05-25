14d646acb3fdf7835ee3b57fd03a25e5178fc0be
Kyne2007 <naiping.hz@qq.com>
2020-05-25 23:58:58 +0800

    dp

--- LeetCode_go/123.go
+++ LeetCode_go/123.go
@@ -15,21 +15,27 @@ func max(x, y int) int {
 }
 
 func maxProfit(prices []int) int {
-	find := func(prices []int) int {
-		if len(prices) == 0 {
-			return 0
-		}
-		ans, m := 0, prices[0]
-		for _, price := range prices {
-			ans = max(ans, price-m)
-			m = min(m, price)
-		}
-		return ans
+	n := len(prices)
+	if n == 0 {
+		return 0
+	}
+	p1 := make([]int, n+1)
+	p2 := make([]int, n+1)
+
+	m := prices[0]
+	for i := 1; i < n; i++ {
+		p1[i] = max(p1[i-1], prices[i]-m)
+		m = min(m, prices[i])
+	}
+	m = prices[n-1]
+	for i := n - 1; i >= 0; i-- {
+		p2[i] = max(p2[i+1], m-prices[i])
+		m = max(m, prices[i])
 	}
 
 	ans := 0
-	for i := 1; i < len(prices); i++ {
-		ans = max(ans, find(prices[0:i])+find(prices[i:]))
+	for i := 0; i < n; i++ {
+		ans = max(ans, p1[i]+p2[i+1])
 	}
 
 	return ans
