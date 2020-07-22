62eaf2558b48c3b499bbebd8a1e6c287d18331b8
Kyne2007 <naiping.hz@qq.com>
2020-07-22 22:01:35 +0800

    dp

--- LeetCode_go/787.go
+++ LeetCode_go/787.go
@@ -1,38 +1,40 @@
 package main
 
-import "fmt"
+import (
+	"math"
+)
+
+func min(x, y int) int {
+	if x < y {
+		return x
+	}
+	return y
+}
 
 func findCheapestPrice(n int, flights [][]int, src int, dst int, K int) int {
-	dp := make([][][]int, n)
-	for i := range dp {
-		dp[i] = make([][]int, n)
-		for j := range dp[i] {
-			dp[i][j] = make([]int, n)
+	dis := make([][]int, n)
+	for i := range dis {
+		dis[i] = make([]int, n)
+		for j := range dis[i] {
+			dis[i][j] = math.MaxInt32
 		}
+		dis[i][src] = 0
 	}
 
-	for _, flight := range flights {
-		dp[flight[0]][flight[1]][0] = flight[2]
-	}
-
-	for k := 1; k <= K; k++ {
-		for i := 0; i < n; i++ {
-			for j := 0; j < n; j++ {
-				dp[i][j][k] = dp[i][j][k-1]
-				for m := 0; m < n; m++ {
-					if dp[i][m][k-1] != 0 && dp[m][j][0] != 0 {
-						if dp[i][j][k] == 0 || dp[i][j][k] > dp[i][m][k-1]+dp[m][j][0] {
-							dp[i][j][k] = dp[i][m][k-1] + dp[m][j][0]
-						}
-					}
+	for k := 0; k <= K; k++ {
+		for _, flight := range flights {
+			if k == 0 {
+				if flight[0] == src {
+					dis[k][flight[1]] = flight[2]
 				}
+				continue
 			}
+			dis[k][flight[1]] = min(dis[k][flight[1]], dis[k-1][flight[0]]+flight[2])
 		}
 	}
-	fmt.Println(dp)
 
-	if dp[src][dst][K] == 0 {
+	if dis[K][dst] == math.MaxInt32 {
 		return -1
 	}
-	return dp[src][dst][K]
+	return dis[K][dst]
 }
