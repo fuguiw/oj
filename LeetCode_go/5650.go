ac3e1759c9d79998ca674ab6bd661a350f4c7535
Kyne2007 <naiping.hz@qq.com>
2021-01-10 14:21:41 +0800

    DSU

--- LeetCode_go/5650.go
+++ LeetCode_go/5650.go
@@ -1,17 +1,24 @@
 package main
 
+func max(x, y int) int {
+	if x > y {
+		return x
+	}
+	return y
+}
+
 func minimumHammingDistance(source []int, target []int, allowedSwaps [][]int) int {
-	fa := make([]int, 100001)
+	fa := make([]int, 1e5+1)
 	for i := range fa {
 		fa[i] = i
 	}
 
 	var find func(int) int
 	find = func(x int) int {
-		if fa[x] == x {
-			return x
+		if fa[x] != x {
+			fa[x] = find(fa[x])
 		}
-		return find(fa[x])
+		return fa[x]
 	}
 
 	merge := func(i, j int) {
@@ -22,42 +29,25 @@ func minimumHammingDistance(source []int, target []int, allowedSwaps [][]int) in
 		merge(swap[0], swap[1])
 	}
 
-	m := make(map[int][]int)
-	visit := make(map[int]bool)
-
-	for _, swap := range allowedSwaps {
-		for i := range swap {
-			f := find(swap[i])
-			if f != swap[i] {
-				m[f] = append(m[f], swap[i])
-			}
-		}
+	n := len(source)
+	d := make([][]int, n)
+	for i := range source {
+		f := find(i)
+		d[f] = append(d[f], i)
 	}
 
 	ans := 0
-	for k, v := range m {
-		v = append(v, k)
-		var arr []int
-		e := make(map[int]int)
-		for _, i := range v {
-			if visit[i] == false {
-				arr = append(arr, i)
-				e[source[i]]++
-			}
-			visit[i] = true
-		}
-		for _, i := range arr {
-			if e[target[i]] <= 0 {
-				ans++
-			} else {
-				e[target[i]]--
-			}
-		}
-	}
-
 	for i := range source {
-		if visit[i] == false && source[i] != target[i] {
-			ans++
+		if len(d[i]) == 0 {
+			continue
+		}
+		A, B := make(map[int]int), make(map[int]int)
+		for _, x := range d[i] {
+			A[source[x]]++
+			B[target[x]]++
+		}
+		for k := range B {
+			ans += max(0, B[k]-A[k])
 		}
 	}
 
