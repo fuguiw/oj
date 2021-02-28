625cd6489c9b8b19578384bed8ccd9f2136f8b75
Kyne2007 <naiping.hz@qq.com>
2021-02-28 22:50:40 +0800

    sort

--- LeetCode_go/896.go
+++ LeetCode_go/896.go
@@ -1,32 +1,7 @@
 package main
 
+import "sort"
+
 func isMonotonic(A []int) (ans bool) {
-	monotonic := func(compare func(x, y int) bool) bool {
-		for i := 1; i < len(A); i++ {
-			if !compare(A[i-1], A[i]) {
-				return false
-			}
-		}
-		return true
-	}
-
-	if monotonic(func(x, y int) bool {
-		if y >= x {
-			return true
-		}
-		return false
-	}) {
-		return true
-	}
-
-	if monotonic(func(x, y int) bool {
-		if y <= x {
-			return true
-		}
-		return false
-	}) {
-		return true
-	}
-
-	return false
+	return sort.IsSorted(sort.IntSlice(A)) || sort.IsSorted(sort.Reverse(sort.IntSlice(A)))
 }
