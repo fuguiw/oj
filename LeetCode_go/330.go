823d0aa22180da13764cc99da644ad426c4afd59
Kyne2007 <naiping.hz@qq.com>
2021-03-21 00:28:48 +0800

    greedy

--- LeetCode_go/330.go
+++ LeetCode_go/330.go
@@ -1,24 +1,15 @@
 package main
 
 func minPatches(nums []int, n int) int {
-	m := 1
 	ans := 0
-	for _, num := range nums {
-		for num > m && m < n {
-			m *= 2
+	for i, x := 0, 1; x <= n; {
+		if i < len(nums) && nums[i] <= x {
+			x += nums[i]
+			i++
+		} else {
+			x *= 2
 			ans++
 		}
-		m += num
-		if m >= n {
-			break
-		}
-	}
-	for m <= n/2 {
-		ans++
-		m *= 2
-	}
-	if m < n {
-		ans++
 	}
 
 	return ans
