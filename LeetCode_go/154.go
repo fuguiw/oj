2c0c6ddb454a974a036fd53364ee5f7116e3a17c
Kyne2007 <naiping.hz@qq.com>
2021-04-09 23:25:44 +0800

    binary search

---
+++ LeetCode_go/154.go
@@ -0,0 +1,16 @@
+package main
+
+func findMin(nums []int) int {
+	left, right := 0, len(nums)-1
+	for left < right {
+		mid := (left + right) >> 1
+		if nums[mid] < nums[right] {
+			right = mid
+		} else if nums[mid] > nums[right] {
+			left = mid + 1
+		} else {
+			right--
+		}
+	}
+	return nums[left]
+}
