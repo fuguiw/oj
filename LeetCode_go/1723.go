1c7d5b30709703cdfbf8eeb81fdc591d38ca9059
Kyne2007 <naiping.hz@qq.com>
2021-05-08 23:18:05 +0800

    dp

--- LeetCode_go/1723.go
+++ LeetCode_go/1723.go
@@ -1,6 +1,8 @@
 package main
 
-import "math"
+import (
+	"math"
+)
 
 func max(x, y int) int {
 	if x > y {
@@ -13,16 +15,17 @@ func min(x, y int) int {
 }
 
 func minimumTimeRequired(jobs []int, k int) int {
+	// dp[i][j] = min(dp[i][j] + max(dp[i-1][k], tot[left])
+
 	n := len(jobs)
 	tot := make([]int, 1<<n)
-	for i := 1; i < 1<<n; i++ {
+	for i := 0; i < 1<<n; i++ {
 		for j := 0; j < n; j++ {
 			if i&(1<<j) == 0 {
 				continue
 			}
 			left := i - (1 << j)
 			tot[i] = tot[left] + jobs[j]
-			break
 		}
 	}
 
@@ -30,20 +33,21 @@ func minimumTimeRequired(jobs []int, k int) int {
 	for i := range dp {
 		dp[i] = make([]int, 1<<n)
 	}
-	for i := range dp[0] {
-		dp[0][i] = tot[i]
-	}
 
-	for j := 1; j < k; j++ {
-		for i := 0; i < 1<<n; i++ {
+	for i := 0; i < k; i++ {
+		for j := range dp[i] {
+			if i == 0 {
+				dp[i][j] = tot[j]
+				continue
+			}
 			minv := math.MaxInt32
-			for s := i; s != 0; s = (s - 1) & i {
-				left := i - s
-				val := max(dp[j-1][left], tot[s])
+			for s := j; s != 0; s = (s - 1) & j {
+				left := j - s
+				val := max(dp[i-1][s], tot[left])
 				minv = min(minv, val)
 			}
-			dp[j][i] = minv
+			dp[i][j] = minv
 		}
 	}
-	return dp[k-1][(1<<n)-1]
+	return dp[k-1][1<<n-1]
 }
