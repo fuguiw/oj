91dc72a67fa46826467186a6d9aca404f082318f
Kyne2007 <naiping.hz@qq.com>
2021-06-16 23:00:55 +0800

    binary search

--- LeetCode_go/852.go
+++ LeetCode_go/852.go
@@ -3,19 +3,7 @@ package main
 import "sort"
 
 func peakIndexInMountainArray(arr []int) int {
-	return sort.Search(len(arr), func(i int) bool {
-		if i == 0 {
-			return false
-		}
-		if i == len(arr)-1 {
-			return true
-		}
-		if arr[i] > arr[i-1] && arr[i] < arr[i+1] {
-			return false
-		}
-		if arr[i] < arr[i-1] && arr[i] > arr[i+1] {
-			return true
-		}
-		return true
+	return sort.Search(len(arr)-1, func(i int) bool {
+		return arr[i] > arr[i+1]
 	})
 }
